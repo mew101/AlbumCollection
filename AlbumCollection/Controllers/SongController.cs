c355bc02b218d292bb8c50ceacad19f3ded274c2
Mo <mshwss@gmail.com>
2019-03-08 15:29:09 -0500

    Added to Controllers

--- AlbumCollection/Controllers/SongController.cs
+++ AlbumCollection/Controllers/SongController.cs
@@ -2,13 +2,23 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using AlbumCollection.Repositories;
 using Microsoft.AspNetCore.Mvc;
 
 namespace AlbumCollection.Controllers
 {
     public class SongController : Controller
     {
-        public IActionResult Index()
+        ISongRepository songRepo;
+
+        public SongController(ISongRepository songRepo)
+        {
+            this.songRepo = songRepo;
+
+        }
+
+
+        public ViewResult Index()
         {
             return View();
         }
--- AlbumCollection/Models/Album.cs
+++ AlbumCollection/Models/Album.cs
@@ -7,7 +7,11 @@ namespace AlbumCollection.Models
 {
     public class Album
     {
-        public int Id { get; set; }
+        public int AlbumId { get; set; }
         public string Name { get; set; }
+        public string Artist { get; set; }
+
+        public virtual IEnumerable<Song> Songs { get; set; }
+
     }
 }
