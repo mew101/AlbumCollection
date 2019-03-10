908536b2133d23b68929cd85c2103582c33043b3
Mo <mshwss@gmail.com>
2019-03-09 21:33:14 -0500

    adding test

--- AlbumCollection.Tests/AlbumControllerTests.cs
+++ AlbumCollection.Tests/AlbumControllerTests.cs
@@ -1,10 +1,37 @@
-﻿using System;
+﻿using AlbumCollection.Controllers;
+using AlbumCollection.Models;
+using AlbumCollection.Repositories;
+using NSubstitute;
+using System;
 using System.Collections.Generic;
 using System.Text;
+using Xunit;
+using Microsoft.AspNetCore.Mvc;
 
 namespace AlbumCollection.Tests
 {
-    class AlbumControllerTests
+    public class AlbumControllerTests
     {
+        AlbumController underTest;
+        private IAlbumRepository repo;
+
+        public AlbumControllerTests()
+        {
+            repo = Substitute.For<IAlbumRepository>();
+
+            underTest = new AlbumController(repo);
+        }
+
+        [Fact]
+        public void Index_Sets_Model_To_All_Albums()
+        {
+            var expectedModel = new List<Album>();
+            repo.GetAll().Returns(expectedModel);
+            var actualModel = underTest.Index().Model;
+
+
+            Assert.Equal(expectedModel, actualModel);
+
+        }
     }
 }
