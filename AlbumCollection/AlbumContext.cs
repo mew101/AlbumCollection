2985e27aa382e2355000a12032bbc46ec8535b53
Mo <mshwss@gmail.com>
2019-03-10 11:13:24 -0400

    AddedAlbumData

--- AlbumCollection/AlbumContext.cs
+++ AlbumCollection/AlbumContext.cs
@@ -14,13 +14,38 @@ namespace AlbumCollection
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            var connectionString = "Server=(localdb)\\mssqllocaldb;Database=MusicTesting;Trusted_Connection=True;";
+            var connectionString = "Server=(localdb)\\mssqllocaldb;Database=AlbumCollection;Trusted_Connection=True;";
 
             optionsBuilder.UseSqlServer(connectionString)
                           .UseLazyLoadingProxies();
 
             base.OnConfiguring(optionsBuilder);
         }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Album>().HasData(
+                new Album()
+                {
+                    AlbumId = 1,
+                    Name = "Disney's Lullaby Album",
+                    Artist = "Fred Mollin & Greg Diakun",
+                    ImgPath = "/Images/DisneyLullaby.png"
+                },
+                new Album()
+                {
+                    AlbumId = 2,
+                    Name = "Sing - A - Longs & Lullabies for the Film Curious George",
+                    Artist = "Jack Johnson",
+                    ImgPath = "/Images/CuriousGeorge.png",
+                },
+                new Album()
+                {
+                    AlbumId = 3,
+                    Name = "Cabbage Patch",
+                    Artist = "Parkers Friends",
+                    ImgPath = "/Images/CabbagePatch.png",
+                });
+                }
     }
 
 }
---
+++ AlbumCollection/Migrations/20190310150917_FirstMigration.cs
@@ -0,0 +1,112 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace AlbumCollection.Migrations
+{
+    public partial class FirstMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Id",
+                table: "Songs",
+                newName: "SongId");
+
+            migrationBuilder.RenameColumn(
+                name: "Id",
+                table: "Albums",
+                newName: "AlbumId");
+
+            migrationBuilder.AddColumn<int>(
+                name: "AlbumId",
+                table: "Songs",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Artist",
+                table: "Albums",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "ImgPath",
+                table: "Albums",
+                nullable: true);
+
+            migrationBuilder.InsertData(
+                table: "Albums",
+                columns: new[] { "AlbumId", "Artist", "ImgPath", "Name" },
+                values: new object[] { 1, "Fred Mollin & Greg Diakun", "/Images/DisneyLullaby.png", "Disney's Lullaby Album" });
+
+            migrationBuilder.InsertData(
+                table: "Albums",
+                columns: new[] { "AlbumId", "Artist", "ImgPath", "Name" },
+                values: new object[] { 2, "Jack Johnson", "/Images/CuriousGeorge.png", "Sing - A - Longs & Lullabies for the Film Curious George" });
+
+            migrationBuilder.InsertData(
+                table: "Albums",
+                columns: new[] { "AlbumId", "Artist", "ImgPath", "Name" },
+                values: new object[] { 3, "Parkers Friends", "/Images/CabbagePatch.png", "Cabbage Patch" });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Songs_AlbumId",
+                table: "Songs",
+                column: "AlbumId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Songs_Albums_AlbumId",
+                table: "Songs",
+                column: "AlbumId",
+                principalTable: "Albums",
+                principalColumn: "AlbumId",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Songs_Albums_AlbumId",
+                table: "Songs");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Songs_AlbumId",
+                table: "Songs");
+
+            migrationBuilder.DeleteData(
+                table: "Albums",
+                keyColumn: "AlbumId",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Albums",
+                keyColumn: "AlbumId",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Albums",
+                keyColumn: "AlbumId",
+                keyValue: 3);
+
+            migrationBuilder.DropColumn(
+                name: "AlbumId",
+                table: "Songs");
+
+            migrationBuilder.DropColumn(
+                name: "Artist",
+                table: "Albums");
+
+            migrationBuilder.DropColumn(
+                name: "ImgPath",
+                table: "Albums");
+
+            migrationBuilder.RenameColumn(
+                name: "SongId",
+                table: "Songs",
+                newName: "Id");
+
+            migrationBuilder.RenameColumn(
+                name: "AlbumId",
+                table: "Albums",
+                newName: "Id");
+        }
+    }
+}
