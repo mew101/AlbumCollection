153068d5dc4e61db439d1de83e522fbf5c4f630b
Mo <mshwss@gmail.com>
2019-03-10 16:32:40 -0400

    working on Album Details

--- AlbumCollection/Controllers/SongController.cs
+++ AlbumCollection/Controllers/SongController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using AlbumCollection.Models;
 using AlbumCollection.Repositories;
 using Microsoft.AspNetCore.Mvc;
 
@@ -16,11 +17,25 @@ namespace AlbumCollection.Controllers
             this.songRepo = songRepo;
 
         }
-
-
         public ViewResult Index()
         {
             return View();
+        }
+        [HttpGet]
+        public ViewResult Create(int id)
+        {
+            var song = new Song()
+            {
+                SongId = id
+            };
+            return View(song);
+        }
+
+        [HttpPost]
+        public ActionResult Create(Song song)
+        {
+            songRepo.Create(song);
+            return RedirectToAction("../Song/Details/" + song.SongId);
         }
     }
 }
